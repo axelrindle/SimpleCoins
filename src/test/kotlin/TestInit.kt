c08506d66e2352665cc0246d5309820a8f93ee7b
Axel Rindle <mail@axelrindle.de>
2020-08-26 21:53:49 +0200

    Tweak test config

--- src/test/kotlin/TestInit.kt
+++ src/test/kotlin/TestInit.kt
@@ -1,12 +1,18 @@
 import be.seeseemelk.mockbukkit.MockBukkit
 import be.seeseemelk.mockbukkit.ServerMock
-import de.axelrindle.simplecoins.CoinManager
 import de.axelrindle.simplecoins.SimpleCoins
+import io.kotest.core.config.AbstractProjectConfig
 import io.kotest.core.listeners.ProjectListener
 import io.kotest.core.spec.AutoScan
+import io.kotest.core.test.TestCaseOrder
 
 @AutoScan
-object TestInit : ProjectListener {
+object TestInit : ProjectListener, AbstractProjectConfig() {
+
+    override val parallelism: Int
+        get() = Runtime.getRuntime().availableProcessors()
+
+    override val testCaseOrder: TestCaseOrder = TestCaseOrder.Sequential
 
     private lateinit var server: ServerMock
     private lateinit var plugin: SimpleCoins
